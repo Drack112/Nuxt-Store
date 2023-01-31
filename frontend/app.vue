83048cd3af54661c4562613a50592a9db1a2e9f0
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-31 17:08:47 -0300

    💡 feat: meta tags on app.vue

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,28 @@
     <h1 class="text-red-500 bg-red-300">Hello NuxtJS</h1>
   </div>
 </template>
+
+<script setup>
+useHead({
+  link: [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      href: "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap",
+      rel: "stylesheet",
+    },
+    { rel: "preconnect", href: "https://app.snipcart.com" },
+    { rel: "preconnect", href: "https://cdn.snipcart.com" },
+    {
+      rel: "stylesheet",
+      href: "https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css",
+    },
+  ],
+  script: [
+    {
+      src: "https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js",
+      async: true,
+    },
+  ],
+});
+</script>
