82cb41e14440654cbeead2e46d1693bdb1b83276
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-31 17:59:54 -0300

    📥 feat: useFetch for the API with v-for

---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-for="product in products.data" v-bind:key="product">
+    <NuxtLink class="font-bold" :to="`/products/${product.id}`">{{
+      product.attributes.Title
+    }}</NuxtLink>
+    <p>{{ product.attributes.Description }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data: products } = await useFetch(
+  () => "http://127.0.0.1:1337/api/products"
+);
+</script>
