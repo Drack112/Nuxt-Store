997dbdf41bd28b2aa405ed62f2da7b10773a714a
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-01 16:57:16 -0300

    🗓 feat: update FooterSection

--- frontend/components/FooterSection.vue
+++ frontend/components/FooterSection.vue
@@ -2,32 +2,37 @@
   <footer class="py-12 text-white bg-brand-grey-800">
     <Container class="grid grid-cols-12 gap-6">
       <div class="col-span-12 md:col-span-6">
-        <img :src="LogoWhite" class="w-32" alt="Pick A Sick Wick" />
+        <img :src="LogoWhite" class="w-32" alt="Pick a Sick Wick" />
         <p class="py-4 md:w-4/5">
-          Pick A Sick Wick is a JAMStack demo site built with Nuxt3, Strapi 4
-          and Snipcart
+          Pick a Sick Wick is a JAMStack demo site built with Nuxt 3, Strapi 4
+          and Snipcart. If you are interested in how it was built, find out more
+          here: JAMStack E-commerce with Nuxt 3, Strapi 4, and Snipcart.
         </p>
       </div>
       <div class="col-span-12 md:col-span-3">
         <Heading tag="h4" font-style="h4" color="text-white">Menu</Heading>
         <ul>
           <li class="underline list-disc list-inside">
-            <NuxtLink>About</NuxtLink>
+            <NuxtLink to="/">Home</NuxtLink>
           </li>
           <li class="underline list-disc list-inside">
-            <NuxtLink>Shop</NuxtLink>
+            <NuxtLink to="/">About</NuxtLink>
           </li>
           <li class="underline list-disc list-inside">
-            <NuxtLink>Contact</NuxtLink>
+            <NuxtLink to="/shop">Shop</NuxtLink>
+          </li>
+          <li class="underline list-disc list-inside">
+            <NuxtLink to="/contact">Contact</NuxtLink>
           </li>
         </ul>
       </div>
       <div class="col-span-12 md:col-span-3">
         <Heading tag="h4" font-style="h4" color="text-white">Contact</Heading>
         <p>The Wick Factory,</p>
-        <p>Flame lane,</p>
+        <p>Flame Lane,</p>
         <p>Waxiton,</p>
-        <p>United Kindgom</p>
+        <p>BN1 5TB</p>
+        <p>United Kingdom</p>
       </div>
     </Container>
   </footer>
