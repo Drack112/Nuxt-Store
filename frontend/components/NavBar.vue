16d9dd4c38772b989c5fe500cbc2e3c78356cecf
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-31 23:26:54 -0300

    🏁 feat: NavBar component

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <ul class="flex flex-col items-center justify-center text-center gap-6">
+      <li
+        class="text-xl font-bold tracking-tighter transition-colors text-brand-grey-800 font-heading hover:text-orange-600"
+      >
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li
+        class="text-xl font-bold tracking-tighter transition-colors text-brand-grey-800 font-heading hover:text-orange-600"
+      >
+        <NuxtLink to="/">Shop</NuxtLink>
+      </li>
+      <li
+        class="text-xl font-bold tracking-tighter transition-colors text-brand-grey-800 font-heading hover:text-orange-600"
+      >
+        <NuxtLink to="/">Contact</NuxtLink>
+      </li>
+      <li
+        class="text-xl font-bold tracking-tighter transition-colors text-brand-grey-800 font-heading hover:text-orange-600"
+      >
+        <NuxtLink to="/">GitHub</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.router-link-active {
+  @apply text-brand-orange;
+}
+</style>
