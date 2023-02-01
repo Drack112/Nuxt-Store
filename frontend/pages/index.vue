aba1c9c9cbd14c5263e395bbbc2843e843d8f7e2
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-01 14:43:36 -0300

    🗒 feat: add the header image, button and more content on index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -10,12 +10,37 @@
       />
     </div>
     <div class="relative z-20 h-full header-content">
-      <div
-        class="absolute flex flex-col justify-center p-8 mx-auto hero-box"
-      ></div>
+      <div class="absolute flex flex-col justify-center p-8 mx-auto hero-box">
+        <div class="relative px-6 py-8 bg-white rounded-lg drop-shadow-2xl">
+          <Heading tag="h3" font-style="h3">Smelly Candles</Heading>
+          <Heading tag="h2" font-style="h2">Only The Sickest Wicks</Heading>
+          <p>
+            If you're like us and can't get enough of burning things in your own
+            home, pick a sick wick.
+          </p>
+          <span class="font-bold"
+            >feed your burning habit safely with added benefit of smelling
+            delicious</span
+          >
+          <Button class="absolute md:-right-3 md:-bottom-3">Shop Now</Button>
+        </div>
+      </div>
     </div>
   </section>
-
+  <section>
+    <Container>
+      <div class="w-full mx-auto mb-12 text-center md:w-2/3 lg:w-1/4">
+        <Heading tag="h2" font-style="h2">Our candles</Heading>
+        <Heading tag="h3" font-style="h3" class="mb-2"
+          >All our candles our hand made 100% verified to satisfy any
+          pyromaniac's itch to burn things in house</Heading
+        >
+      </div>
+    </Container>
+  </section>
+  <div class="flex justify-center mt-10">
+    <Button theme="secondary"> View all the sick wicks </Button>
+  </div>
   <div v-for="product in products.data" v-bind:key="product">
     <NuxtLink class="font-bold" :to="`/products/${product.id}`">{{
       product.attributes.Title
