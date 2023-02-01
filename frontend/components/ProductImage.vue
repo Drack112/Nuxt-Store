ec8c8a0f66b889cc7b0cce3f83c34f25b155ab43
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-01 15:44:17 -0300

    📻 feat: product image component

---
+++ frontend/components/ProductImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-100 shadow-xl aspect-w-1 aspect-h-1">
+    <img :src="imageURL" :alt="product.attributes.Title" loading="lazy" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  product: Object,
+});
+
+const config = useRuntimeConfig();
+const imageURL = computed(() => {
+  return `${config.public.apiBase}${props.product.attribute.Image.data.attributes.formats.medium.url}`;
+});
+</script>
