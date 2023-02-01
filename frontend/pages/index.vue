5f49a27f7c461c688fac049f59fa4b8f4e8f2499
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-01 17:05:50 -0300

    📥 feat: index.vue receiving data of all candles

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,57 +1,67 @@
 <template>
-  <section
-    class="relative mb-20 -mt-24 border-b-4 md:mb-10 header border-brand-beige"
-  >
-    <div class="absolute inset-0 z-0 overflow-hidden">
-      <img
-        :src="HeaderBG"
-        class="object-cover w-full h-full"
-        alt="Pick a sick wick"
-      />
-    </div>
-    <div class="relative z-20 h-full header-content">
-      <div class="absolute flex flex-col justify-center p-8 mx-auto hero-box">
-        <div class="relative px-6 py-8 bg-white rounded-lg drop-shadow-2xl">
-          <Heading tag="h3" font-style="h3">Smelly Candles</Heading>
-          <Heading tag="h2" font-style="h2">Only The Sickest Wicks</Heading>
-          <p>
-            If you're like us and can't get enough of burning things in your own
-            home, pick a sick wick.
-          </p>
-          <span class="font-bold"
-            >feed your burning habit safely with added benefit of smelling
-            delicious</span
-          >
-          <Button class="absolute md:-right-3 md:-bottom-3">Shop Now</Button>
+  <div>
+    <section
+      class="relative mb-20 -mt-24 border-b-4 md:mb-10 header border-brand-beige"
+    >
+      <div class="absolute inset-0 z-0 overflow-hidden">
+        <img
+          :src="HeaderBg"
+          class="object-cover w-full h-full"
+          alt="Pick a sick wick"
+        />
+      </div>
+
+      <div class="relative z-20 h-full header-content">
+        <div class="absolute flex flex-col justify-center p-8 mx-auto hero-box">
+          <div class="relative px-6 py-8 bg-white rounded-lg drop-shadow-2xl">
+            <Heading tag="h3" font-style="h3">Smelly candles</Heading>
+            <Heading tag="h2" font-style="h2">Only the sickest wicks</Heading>
+            <p class="pb-4 pr-4 text-brand-gray font-body">
+              If you’re like us and can’t get enough of burning things in your
+              own home, pick a sick wick and
+              <span class="font-bold"
+                >feed your burning habbit safely with added benifit of smelling
+                delicious.</span
+              >
+            </p>
+            <Btn class="absolute md:-right-3 md:-bottom-3">Shop Now</Btn>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-  <section>
-    <Container>
-      <div class="w-full mx-auto mb-12 text-center md:w-2/3 lg:w-1/4">
-        <Heading tag="h2" font-style="h2">Our candles</Heading>
-        <Heading tag="h3" font-style="h3" class="mb-2"
-          >All our candles our hand made 100% verified to satisfy any
-          pyromaniac's itch to burn things in house</Heading
-        >
-      </div>
-    </Container>
-  </section>
-  <div class="flex justify-center mt-10">
-    <Button theme="secondary"> View all the sick wicks </Button>
-  </div>
-  <div v-for="product in products.data" v-bind:key="product">
-    <NuxtLink class="font-bold" :to="`/products/${product.id}`">{{
-      product.attributes.Title
-    }}</NuxtLink>
-    <p>{{ product.attributes.Description }}</p>
+    </section>
+
+    <section class="mb-28">
+      <Container>
+        <div class="w-full mx-auto mb-12 text-center md:w-2/3 lg:w-1/3">
+          <Heading tag="h2" font-style="h3">Our Candles</Heading>
+          <Heading tag="h3" font-style="h2" class="mb-2"
+            >Show me the sick wicks</Heading
+          >
+          <p>
+            All our candles our hand made and 100% verified to satisfy any
+            pyromaniac’s itch to burn things in a safe way.
+          </p>
+        </div>
+
+        <div class="grid grid-cols-2 gap-12 md:grid-cols-4">
+          <ProductTeaser
+            class="col-span-1"
+            v-for="product in products.data"
+            :key="product.id"
+            :product="product"
+          />
+        </div>
+
+        <div class="flex justify-center mt-10">
+          <Btn theme="secondary">View all the sick wicks</Btn>
+        </div>
+      </Container>
+    </section>
   </div>
 </template>
 
 <script setup>
-import HeaderBG from "assets/images/header-bg.jpg";
-
+import HeaderBg from "assets/images/header-bg.jpg";
 const config = useRuntimeConfig();
 const { data: products } = await useFetch(
   () =>
@@ -60,6 +70,11 @@ const { data: products } = await useFetch(
 </script>
 
 <style scoped>
+.header {
+  height: 600px;
+  /* margin-top: -106px;   */
+}
+
 @screen md {
   .hero-box {
     left: 10%;
@@ -67,7 +82,6 @@ const { data: products } = await useFetch(
     width: 485px;
   }
 }
-
 @screen md {
   .header {
     height: 700px;
@@ -75,7 +89,7 @@ const { data: products } = await useFetch(
 }
 
 @screen md {
-  .home-header-image {
+  .home-head-image {
     top: -110px;
     height: 770px;
     width: 100%;
